4565fd2b983bf78c189546b43e798422c394280e
dharmateja03 <59060125+dharmateja03@users.noreply.github.com>
2021-08-27 16:18:44 +0530

    Create Bubble Sort.py

---
+++ Bubble Sort.py
@@ -0,0 +1,10 @@
+#BUBBLE SORT
+#swap adjacent elemnts
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)-1-i):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+#Time -O(n*n)
+#space-O(1)
